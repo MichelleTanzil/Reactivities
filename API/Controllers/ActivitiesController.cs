31069c1505cfad05ed00bcfbeac57bcb047ad8ad
Michelle Tanzil <mtanzil01@gmail.com>
2020-04-04 21:31:31 -0700

    End of section 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -43,19 +43,17 @@ namespace API.Controllers
       return await _mediator.Send(command);
     }
 
-    // [HttpPut("{id}")]
-    // public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
-    // {
-    //   command.Id = id;
-    //   return await _mediator.Send(command);
-    // }
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+    {
+      command.Id = id;
+      return await _mediator.Send(command);
+    }
 
-    // [HttpDelete("{id}")]
-    // public async Task<ActionResult<Unit>> Delete(Guid id)
-    // {
-    //   return await _mediator.Send(new Delete.Command { Id = id });
-    // }
-
-    // You do return await _mediator.Send(command); when you need to create/update and you do return await _mediator.Send(new Delete.Command{Id = id}); when you just get the object, but not change what inside of it.
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Unit>> Delete(Guid id)
+    {
+      return await _mediator.Send(new Delete.Command { Id = id });
+    }
   }
 }
