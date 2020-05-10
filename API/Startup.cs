de354f082c856a9c372dc75c2864b0b32b17a315
Michelle Tanzil <mtanzil01@gmail.com>
2020-05-10 09:39:51 -0700

    End of section 12

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using API.Controllers;
 using API.Middleware;
 using Application.Activities;
 using Application.Interfaces;
@@ -9,16 +11,20 @@ using Domain;
 using FluentValidation.AspNetCore;
 using Infrastructure.Security;
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Persistence;
 
 namespace API
@@ -41,22 +47,23 @@ namespace API
             opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
           });
       services.AddMediatR(typeof(List.Handler).Assembly);
-      services
-          .AddCors(opt =>
+      services.AddCors(opt =>
+        {
+          opt.AddPolicy("CorsPolicy", policy =>
           {
-            opt
-                .AddPolicy("CorsPolicy",
-                policy =>
-                {
-                  policy
-                            .AllowAnyHeader()
-                            .AllowAnyMethod()
-                            .WithOrigins("http://localhost:3000");
-                });
+            policy
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .WithOrigins("http://localhost:3000");
           });
+        });
 
       services
-          .AddMvc()
+          .AddControllers(opt =>
+          {
+            var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+            opt.Filters.Add(new AuthorizeFilter(policy));
+          })
           .AddFluentValidation(cfg =>
           {
             cfg.RegisterValidatorsFromAssemblyContaining<Create>();
@@ -67,8 +74,22 @@ namespace API
       identityBuilder.AddEntityFrameworkStores<DataContext>();
       identityBuilder.AddSignInManager<SignInManager<AppUser>>();
 
-      services.AddAuthentication();
+      var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
+
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+      .AddJwtBearer(opt =>
+      {
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+          ValidateIssuerSigningKey = true,
+          IssuerSigningKey = key,
+          ValidateAudience = false,
+          ValidateIssuer = false
+        };
+      });
+
       services.AddScoped<IJwtGenerator, JwtGenerator>();
+      services.AddScoped<IUserAccessor, UserAccessor>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -84,8 +105,11 @@ namespace API
       // app.UseHttpsRedirection();
       // Remove https://localhost:5001 for now from launchSettings.json
       app.UseRouting();
-      app.UseAuthorization();
       app.UseCors("CorsPolicy");
+
+      app.UseAuthentication();
+      app.UseAuthorization();
+
       app
           .UseEndpoints(endpoints =>
           {
