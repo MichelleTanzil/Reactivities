04ae434e62b02cb51726e8e37707d0424bdee3d1
Michelle Tanzil <mtanzil01@gmail.com>
2020-05-31 09:32:44 -0700

    Finished lesson 235

---
+++ client/src/app/common/photoUpload/PhotoWidgetDropzone.tsx
@@ -0,0 +1,32 @@
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
+interface IProps {
+  setFiles: (files: object[]) => void;
+}
+
+const PhotoWidgetDropZone: React.FC<IProps> = ({ setFiles }) => {
+  const onDrop = useCallback((acceptedFiles) => {
+    setFiles(
+      acceptedFiles.map((file: object) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file),
+        })
+      )
+    );
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
+};
+
+export default PhotoWidgetDropZone;
--- client/src/features/profiles/ProfileContent.tsx
+++ client/src/features/profiles/ProfileContent.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Tab } from "semantic-ui-react";
+import ProfilePhotos from "./ProfilePhotos";
 
 const panes = [
   { menuItem: "About", render: () => <Tab.Pane>About content</Tab.Pane> },
-  { menuItem: "Photos", render: () => <Tab.Pane>Photos content</Tab.Pane> },
+  { menuItem: "Photos", render: () => <ProfilePhotos /> },
   {
     menuItem: "Activities",
     render: () => <Tab.Pane>Activities content</Tab.Pane>,
@@ -24,6 +25,7 @@ const ProfileContent = () => {
       menu={{ fluid: true, vertical: true }}
       menuPosition="right"
       panes={panes}
+      activeIndex={1}
     />
   );
 };
