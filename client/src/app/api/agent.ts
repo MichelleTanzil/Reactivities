e2e2d288bcec0155b16b750baaea1406c5e413ed
Michelle Tanzil <mtanzil01@gmail.com>
2020-06-01 06:57:02 -0700

    Finished lesson 239

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -3,7 +3,7 @@ import { IActivity } from "../models/activity";
 import { history } from "../..";
 import { toast } from "react-toastify";
 import { IUser, IUserFormValues } from "../models/user";
-import { IProfile } from "../models/profile";
+import { IProfile, IPhoto } from "../models/profile";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
@@ -54,6 +54,15 @@ const requests = {
   put: (url: string, body: {}) =>
     axios.put(url, body).then(sleep(500)).then(responseBody),
   del: (url: string) => axios.delete(url).then(sleep(500)).then(responseBody),
+  postForm: (url: string, file: Blob) => {
+    let formData = new FormData();
+    formData.append("File", file);
+    return axios
+      .post(url, formData, {
+        headers: { "Content-type": "multipart/form-data" },
+      })
+      .then(responseBody);
+  },
 };
 
 const Activities = {
@@ -78,6 +87,8 @@ const User = {
 const Profiles = {
   get: (username: string): Promise<IProfile> =>
     requests.get(`/profiles/${username}`),
+  uploadPhoto: (photo: Blob): Promise<IPhoto> =>
+    requests.postForm(`/photos`, photo),
 };
 
 export default {
--- client/src/app/common/photoUpload/PhotoWidgetDropzone.tsx
+++ client/src/app/common/photoUpload/PhotoWidgetDropzone.tsx
@@ -1,9 +1,23 @@
 import React, { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
+import { Icon, Header } from "semantic-ui-react";
 
 interface IProps {
   setFiles: (files: object[]) => void;
 }
+
+const dropzoneStyles = {
+  border: "dashed 3px",
+  borderColor: "#eee",
+  borderRadius: "5px",
+  paddingTop: "30px",
+  textAlign: "center" as "center",
+  height: "200px",
+};
+
+const dropzoneActive = {
+  borderColor: "green",
+};
 
 const PhotoWidgetDropZone: React.FC<IProps> = ({ setFiles }) => {
   const onDrop = useCallback((acceptedFiles) => {
@@ -18,13 +32,15 @@ const PhotoWidgetDropZone: React.FC<IProps> = ({ setFiles }) => {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
+    <div
+      {...getRootProps()}
+      style={
+        isDragActive ? { ...dropzoneStyles, ...dropzoneActive } : dropzoneStyles
+      }
+    >
       <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      )}
+      <Icon name="upload" size="huge" />
+      <Header content="Drop image here" />
     </div>
   );
 };
